7c8b7941a2108a9a2548f9fc9a616de568a3cc24
Omar Davide Xavier <117167127+OmarDavideXavier@users.noreply.github.com>
2026-03-04 22:05:28 +0200

    Meus arquivos do Django

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class Post(admin.ModelAdmin):
+    list_display = ("author", "title", "text", "created_date", "published_date")
+
+
+
