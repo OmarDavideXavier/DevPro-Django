74e0be168332417f670f1c3210b5f6877ea1bdd1
Omar Davide Xavier <omardavidexavier@gmail.com>
2026-03-10 15:40:05 +0200

    Agora templates

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from blog import views
+
+urlpatterns = [
+   # path('', views.post_list, name='post_list'),
+   path('portao', views.portao),
+   path('sala', views.sala),
+   path('quarto', views.quarto),
+  
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def portao(request):
+    return HttpResponse("Voce chegou ao portao da casa")
+
+def sala(request):
+    return HttpResponse("Voce chegou a sala")
+
+def quarto(request):
+    return HttpResponse("Voce chegou ao quarto")
